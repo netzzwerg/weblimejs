765cf664287c8c09aca8324dfeebf25ac7bd7794
Béla Varga <bvarga@chip.de>
2014-05-28 19:01:05 +0200

    Changed code style.

--- app/app.js
+++ app/app.js
@@ -1,46 +1,42 @@
 (function() {
-'use strict';
+  'use strict';
 
-	console.log('app init');
+  requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+      app: '../app'
+    }
+  });
 
-	requirejs.config({
-		baseUrl: 'lib',
-		paths: {
-			app: '../app'
-		}
-	});
+  require(['jquery', 'weblime'], function($, Weblime) {
 
-	require(['jquery','weblime'],function($,Weblime) {
+    new Weblime({
+      el: $('.weblime')[0],
+      theme: 'textmate', // default theme is textmate
+      path: 'source/',
+      callback: function(weblime) {
+        console.log(weblime);
+      }
+    });
 
-		console.log('loaded');
+    new Weblime({
+      el: $('.weblime')[1],
+      theme: 'letterpress',
+      path: 'source/',
+      callback: function(weblime) {
+        console.log(weblime);
+      }
+    });
 
-		new Weblime({
-			el: $('.weblime')[0],
-			theme: 'textmate', // default theme is textmate
-			path: 'source/',
-			callback: function(weblime) {
-				console.log(weblime);
-			}
-		});
+    new Weblime({
+      el: $('.weblime')[2],
+      theme: 'monokai',
+      path: 'source/',
+      callback: function(weblime) {
+        console.log(weblime);
+      }
+    });
 
-		new Weblime({
-			el: $('.weblime')[1],
-			theme: 'letterpress',
-			path: 'source/',
-			callback: function(weblime) {
-				console.log(weblime);
-			}
-		});
-
-		new Weblime({
-			el: $('.weblime')[2],
-			theme: 'monokai',
-			path: 'source/',
-			callback: function(weblime) {
-				console.log(weblime);
-			}
-		});
-
-	});
+  });
 
 }());
